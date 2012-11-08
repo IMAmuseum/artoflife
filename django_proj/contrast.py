2f2434f37b5f85d10da560207d58f6bd83349f1a
Ed Bachta <ebachta@imamuseum.org>
2012-11-08 10:49:29 -0500

    Implemented j2k versions of the contrast and compression techniques

--- django_proj/contrast.py
+++ django_proj/contrast.py
@@ -89,6 +89,80 @@ def processPage(scan_id, ia_page_index):
     return info
 
 
+def processImage(img, scan_id, ia_page_index, pct_thresh=10):
+
+    if not os.path.exists('tmp/contrast'):
+        os.mkdir('tmp/contrast')
+    if not os.path.exists('tmp/contrast/%s' % (scan_id)):
+        os.mkdir('tmp/contrast/%s' % (scan_id))
+
+    t0 = clock()
+
+    working_file = 'tmp/contrast/%s/%s_contrast_%s.png' % (scan_id, scan_id, ia_page_index)
+
+    img.save(working_file)
+
+    # desaturate
+    convert(working_file, '-colorspace Gray')
+    #img = img.convert('L')
+
+    # apply heavy contrast
+    convert(working_file, '-contrast -contrast -contrast -contrast -contrast -contrast -contrast -contrast')
+    #img = ImageOps.autocontrast(img, 5)
+
+    t1 = clock()
+
+    processing_size = 500
+
+    # resize to 1px width
+    convert(working_file, '-resize 1x%d!' % (processing_size))
+    #w_compressed = ImageOps.equalize(img.resize((1, size[1]), Image.BILINEAR))
+
+    # sharpen
+    convert(working_file, '-sharpen 0x5')
+    #enhancer = ImageEnhance.Sharpness(w_compressed)
+    #w_compressed = enhancer.enhance(2.0)
+
+    # convert to grayscale
+    convert(working_file, '-negate -threshold 0 -negate')
+
+    # identify long lines
+    w_compressed = Image.open(working_file)
+
+    info = {
+        'max_contiguous': 0,
+        'image_detected': False,
+        'total_time': 0,
+        '1d_time': 0
+    }
+
+    pixel_data = list(w_compressed.getdata())
+    #print pixel_data
+    n_contig = 0
+    for p in pixel_data:
+        if p == 0:
+            n_contig += 1
+        else:
+            info['max_contiguous'] = max(info['max_contiguous'], n_contig)
+            n_contig = 0
+
+    del w_compressed
+
+    info['max_contiguous'] = float(info['max_contiguous']) / processing_size
+
+    if (info['max_contiguous'] > pct_thresh / 100.0):
+        info['image_detected'] = True
+
+    tf = clock()
+
+    info['total_time'] = tf - t0
+    info['1d_time'] = tf - t1
+
+    return info
+
+
+
+
 def runCSV(scan_id, page=None):
 
     scandata_file = 'scandata/%s/%s_scandata.xml' % (scan_id, scan_id)
--- django_proj/process_jp2.py
+++ django_proj/process_jp2.py
@@ -4,39 +4,68 @@ from wand.image import Image as wImage
 from PIL import Image as pImage 
 from cStringIO import StringIO
 
+from contrast import processImage as processContrast
+
+mode_to_bpp = {
+    '1': 1,
+    'L': 8,
+    'P': 8,
+    'RGB': 24,
+    'RGBA': 32,
+    'CMYK': 32,
+    'YCbCr': 24,
+    'I': 32,
+    'F': 32
+}
+
+
+def processPage(book_id, page_id):
+
+    t0 = clock()
+
+    tar_filename = 'scandata/%s/%s_jp2.tar' % (book_id, book_id)
+    tar_file = tarfile.open(tar_filename)
+
+    jp2_file = '%s_jp2/%s_%04d.jp2' % (book_id, book_id, ia_page_index)
+    jp2_info = tar_file.getmember(jp2_file)
+    jp2_data = tar_file.extractfile(jp2_file)
+
+    scale = 0.5
+
+    # Use Wand to read jp2, then pass to PIL as png
+    w_image = wImage(blob=jp2_data)
+    #w_image.resize(int(w_image.width*scale), int(w_image.height*scale)) # Improve performance?
+    image = pImage.open(StringIO(w_image.make_blob('png')))
+
+    t1 = clock()
+    print 'Loaded image in', t1 - t0, 's'
+
+    # Compression analysis
+    raw_size = image.size[0] * image.size[1] * mode_to_bpp[image.mode] / 8
+    compression = float(jp2_info.size) / raw_size
+
+    # Contrast analysis
+    contrast_info = processContrast(image, book_id, page_id)
+
+    print 'Processed in', clock() - t1, 's'
+
+    info = {
+        'width': w_image.width,
+        'height': w_image.height,
+        'depth': w_image.depth,
+        'file_size': jp2_info.size,
+        'raw_size': raw_size,
+        'compression_ratio': compression
+    }
+
+    print info
+    print contrast_info
+
+    del image
+    del w_image
+
+
 book_id = 'mobot31753000121308'
 ia_page_index = 100
 
-t0 = clock()
-
-tar_filename = 'scandata/%s/%s_jp2.tar' % (book_id, book_id)
-tar_file = tarfile.open(tar_filename)
-
-jp2_file = '%s_jp2/%s_%04d.jp2' % (book_id, book_id, ia_page_index)
-jp2_info = tar_file.getmember(jp2_file)
-jp2_data = tar_file.extractfile(jp2_file)
-
-# Use Wand to read jp2, then pass to PIL as png
-w_image = wImage(blob=jp2_data)
-t1 = clock()
-image = pImage.open(StringIO(w_image.make_blob('png')))
-
-print 'Loaded image in', clock()-t0, 's', t1-t0
-
-raw_size = w_image.width*w_image.height*w_image.depth*3/8
-
-
-info = {
-	'width': w_image.width,
-	'height': w_image.height,
-	'depth': w_image.depth,
-	'file_size': jp2_info.size,
-	'raw_size': raw_size,
-	'size_ratio': jp2_info.size / float(raw_size)
-}
-
-print info
-
-
-#image_zip.close()
-#del page_img
+processPage(book_id, ia_page_index)
