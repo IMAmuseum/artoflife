f2e67380dba178eaad3aef26c7449bfe65e734bc
Ed Bachta <ebachta@imamuseum.org>
2012-11-18 14:10:16 -0500

    Added comparative histogram for contrast

---
+++ django_proj/contrast-histogram.py
@@ -0,0 +1,51 @@
+import pymongo
+from analysis import classifyResult
+from helpers import getMongoCollection
+from numpy import array, histogram
+
+if __name__ == '__main__':
+
+    mongo_conn = pymongo.Connection('localhost', 27017)
+    mongo_db = mongo_conn.artoflife
+
+    page_data = mongo_db.page_data
+
+    pages = page_data.find({})
+
+    range_with = (None, None)
+    range_without = (None, None)
+
+    with_v = []
+    without_v = []
+
+    for page in pages:
+
+        if ('contrast' in page) and ('max_contiguous' in page['contrast']):
+            if page['has_illustration']['gold_standard']:
+                with_v.append(page['contrast']['max_contiguous'])
+            else:
+                without_v.append(page['contrast']['max_contiguous'])
+        else:
+            print 'No contrast on', page['scan_id'], page['ia_page_num']
+
+
+    with_v = array(with_v)
+    without_v = array(without_v)            
+
+    print 'Range with: (%f, %f)' % (with_v.min(), with_v.max())
+    print 'Range without: (%f, %f)' % (without_v.min(), without_v.max())
+
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    n, bins, patches = ax.hist([with_v, without_v], 100, color=('green', 'grey'), edgecolor=None, histtype='barstacked')
+
+    ax.set_xlabel('Max contiguous ratio')
+    ax.set_ylabel('Occurrences')
+    #ax.set_title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$')
+    #ax.set_xlim(0, 120)
+    ax.grid(True)
+
+    plt.show()
--- django_proj/contrast.py
+++ django_proj/contrast.py
@@ -18,75 +18,7 @@ def processPage(scan_id, ia_page_index):
 
     img = getIAImage(scan_id, ia_page_index)
 
-    if not os.path.exists('tmp/contrast'):
-        os.mkdir('tmp/contrast')
-    if not os.path.exists('tmp/contrast/%s' % (scan_id)):
-        os.mkdir('tmp/contrast/%s' % (scan_id))
-
-    working_file = 'tmp/contrast/%s/%s_contrast_%s.png' % (scan_id, scan_id, ia_page_index)
-
-    t0 = clock()
-
-    orig_size = img.size
-    scale = 0.25
-    print 'Original image size:', orig_size
-
-    # scale down to improve performance
-    size = (int(orig_size[0] * scale), int(orig_size[1] * scale))
-    print 'working size:', size
-    img.thumbnail(size, Image.ANTIALIAS)
-    img.save(working_file)
-
-    # desaturate
-    convert(working_file, '-colorspace Gray')
-    #img = img.convert('L')
-
-    # apply heavy contrast
-    convert(working_file, '-contrast -contrast -contrast -contrast -contrast -contrast -contrast -contrast')
-    #img = ImageOps.autocontrast(img, 5)
-
-    t1 = clock()
-
-    # resize to 1px width
-    convert(working_file, '-resize 1x500!')
-    #w_compressed = ImageOps.equalize(img.resize((1, size[1]), Image.BILINEAR))
-
-    # sharpen
-    convert(working_file, '-sharpen 0x5')
-    #enhancer = ImageEnhance.Sharpness(w_compressed)
-    #w_compressed = enhancer.enhance(2.0)
-
-    # convert to grayscale
-    convert(working_file, '-negate -threshold 0 -negate')
-
-    # identify long lines
-    w_compressed = Image.open(working_file)
-
-    info = {
-        'image_detected': False
-    }
-
-    pixel_data = list(w_compressed.getdata())
-    #print pixel_data
-    n_contig = 0
-    for p in pixel_data:
-        if p == 0:
-            n_contig += 1
-            if (n_contig > 50):
-                info['image_detected'] = True
-                print 'image detected'
-                break
-        else:
-            n_contig = 0
-
-    del img
-
-    tf = clock()
-
-    benchmarks['image_processing'].append(tf - t0)
-    benchmarks['single_dim'].append(tf - t1)
-
-    return info
+    return processImage(img, scan_id, ia_page_index)
 
 
 def processImage(img, scan_id, ia_page_index, pct_thresh=10):
@@ -161,58 +93,15 @@ def processImage(img, scan_id, ia_page_index, pct_thresh=10):
     return info
 
 
-
-
-def runCSV(scan_id, page=None):
-
-    scandata_file = 'scandata/%s/%s_scandata.xml' % (scan_id, scan_id)
-
-    print 'parsing scandata'
-    t = clock()
-    scandata = ET.parse(scandata_file)
-    scandata_pages = scandata.find('pageData').findall('page')
-    print 'found', len(scandata_pages), 'pages from scan data in', clock() - t, 's'
-
-    if (args.page != None):
-        result = processPage(scan_id, args.page, scandata_pages[args.page])
-        print result
-    else:
-        results = []
-        ia_page_index = 0
-        for i in range(0, len(scandata_pages)):
-            if scandata_pages[i].find('pageType').text == 'Delete':
-                continue
-            if (args.page == None):
-                # process all pages
-                results.append(processPage(scan_id, ia_page_index, scandata_pages[i]))
-            elif (i == args.page):
-                break
-            ia_page_index += 1
-
-    import csv
-    output_filename = 'output/contrast/%s-contrast.csv' % scan_id
-    if not os.path.exists(os.path.dirname(output_filename)):
-        os.mkdir(os.path.dirname(output_filename))
-
-    output_file = open(output_filename, 'w')
-    writer = csv.writer(output_file)
-    writer.writerow(['IA page', 'Image detected', 'Processing time', '1D'])
-
-    for p in range(0, len(results)):
-        writer.writerow([p, results[p]['image_detected'], benchmarks['image_processing'][p], benchmarks['single_dim'][p]])
-        if (results[p]['image_detected']):
-            print 'Image detected on page', p
-
-    output_file.close()
-
-
 def runMongo(scan_id, page=None):
 
     from helpers import getMongoCollection
 
     coll = getMongoCollection('page_data')
 
-    if page is None:
+    if scan_id is None:
+        pages = coll.find({})
+    elif page is None:
         pages = coll.find({'scan_id': scan_id})
     else:
         pages = coll.find({'scan_id': scan_id, 'scandata_index': page})
@@ -221,11 +110,14 @@ def runMongo(scan_id, page=None):
         result = processPage(page['scan_id'], page['ia_page_num'])
 
         page['has_illustration']['contrast'] = result['image_detected']
+        page['contrast'] = result
+        """
         if not 'benchmarks' in page:
             page['benchmarks'] = {
                 'contrast': {}
             }
         page['benchmarks']['contrast']['total'] = benchmarks['image_processing'][-1:][0]
+        """
 
         coll.save(page)
 
@@ -240,7 +132,7 @@ if __name__ == '__main__':
     from xml.etree import cElementTree as ET
 
     ap = argparse.ArgumentParser(description='picture block processing')
-    ap.add_argument('scan', type=str, help='scan id')
+    ap.add_argument('--scan', type=str, help='scan id', default=None)
     ap.add_argument('--page', type=int, help='page #', default=None)
 
     args = ap.parse_args()
