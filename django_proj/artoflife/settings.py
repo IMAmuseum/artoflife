31c103a2b0c346565627293e34cee936668acbe1
Ed Bachta <ebachta@imamuseum.org>
2012-12-27 16:27:03 -0500

    Path fix

--- django_proj/artoflife/settings.py
+++ django_proj/artoflife/settings.py
@@ -50,12 +50,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "media") 
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "files") 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/files/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
